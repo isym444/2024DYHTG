1e04d8c4019cbcb13df54b998906c7ac2a2eaaee
Acul4321 <lucarogers666@gmail.com>
2024-10-12 22:22:38 +0100

    addeed placment functionality#

--- script/main.gd
+++ script/main.gd
@@ -22,15 +22,34 @@ func _process(delta):
 	plane_position = Vector3(round(world_position.x), 0, round(world_position.z))
 	selector.position = lerp(selector.position, plane_position, delta * 40)
 
-func _unhandled_input(event):
+func _input(event):
 	if(Input.is_action_just_pressed("click")):
 		#check for placment conditions
 		if(plane_position.x < -bounds or plane_position.x > bounds or plane_position.z < -bounds or plane_position.z > bounds):
 			print("out of bounds")
 			return
-		
+		var building
 		#instantiate selected building
-		var building = load("res://scene/building/residentialBuilding.tscn").instantiate()
+		match global.currentBuilding:
+			0:
+				building = load("res://scene/building/residentialBuilding.tscn").instantiate()
+				global.add_apartment_building(plane_position.x,plane_position.y,plane_position.z)
+			1:
+				building = load("res://scene/building/investmentBank.tscn").instantiate()
+				global.add_bank(plane_position.x,plane_position.y,plane_position.z,global.bankState.RISKY)
+			2:
+				building = load("res://scene/building/marshallWace.tscn").instantiate()
+				global.add_bank(plane_position.x,plane_position.y,plane_position.z,global.bankState.RISKY)
+			3:
+				building = load("res://scene/building/oilPump.tscn").instantiate()
+				global.add_oil_pumps(plane_position.x,plane_position.y,plane_position.z)
+			4:
+				building = load("res://scene/building/forest.tscn").instantiate()
+				global.add_forest(plane_position.x,plane_position.y,plane_position.z)
+			5:
+				building = load("res://scene/building/windTurbine.tscn").instantiate()
+				global.add_wind_turbine(plane_position.x,plane_position.y,plane_position.z)
+
 		building.global_position = plane_position
 		# Add the TextureRect to the scene
 		add_child(building)
