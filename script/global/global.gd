21ccab854746c16658f8ee335f85f2e9a8c60b07
João Manuel <2958530K@student.gla.ac.uk>
2024-10-12 21:08:29 +0100

    add dashboard with graphs. Add UI with numerical data and building placing buttons

--- script/global/global.gd
+++ script/global/global.gd
@@ -5,6 +5,8 @@ const oil_per_factory = 1 #MB millions of barrels per time
 const materials_per_factory = 5 #building units
 const energy_per_turbine = 5 #GW
 
+var currentBuilding: int = 0
+
 #Resources
 var money = 10000000 #GBP (10 million) #money can go down as non-residential buildings cost money to operate so they can operate at deficit if you don't sell your oil/energy or price of sell is too low
 var energy = 2400 #GW (100GWh average forc city so nough for 24h)
---
+++ script/u_i.gd
@@ -0,0 +1,42 @@
+extends Control
+
+@onready var world_health_label = %worldHealth
+@onready var money_label = %money
+@onready var people_label = %people
+@onready var oil_label = %oil
+@onready var energy_label = %energy
+@onready var materials_label = %materials
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	update_text_labels()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	update_text_labels()
+	
+func update_text_labels() -> void:
+	world_health_label.text = "World health: " + str(global.world_health)
+	money_label.text = "Money: " + str(global.money)
+	people_label.text = "People: " + str(global.people)
+	oil_label.text = "Oil: " + str(global.oil)
+	energy_label.text = "Energy: " + str(global.energy)
+	materials_label.text = "Materials: " + str(global.materials)
+	
+func _on_add_residential_pressed() -> void:
+	global.currentBuilding = 0
+	
+func _on_add_investment_pressed() -> void:
+	global.currentBuilding = 1
+
+func _on_add_marshall_pressed() -> void:
+	global.currentBuilding = 2
+
+func _on_add_oil_pressed() -> void:
+	global.currentBuilding = 3
+
+func _on_add_forest_pressed() -> void:
+	global.currentBuilding = 4
+
+func _on_add_wind_pressed() -> void:
+	global.currentBuilding = 5
