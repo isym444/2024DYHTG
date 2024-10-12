04702b1650917f5bd5560ca202b9d34ac01723ae
Acul4321 <lucarogers666@gmail.com>
2024-10-12 21:08:19 +0100

    created bounds and start/end screen

---
+++ script/startMenu.gd
@@ -0,0 +1,19 @@
+extends Control
+var main = preload("res://scene/main.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_packed(main)
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
