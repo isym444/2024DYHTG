6897d11c09a986cb1de602e746313279e3a5d112
isym444 <samir.yep@gmail.com>
2024-10-12 17:14:31 +0100

    added functions to add buildings

--- script/global/global.gd
+++ script/global/global.gd
@@ -24,7 +24,7 @@ var bank_template = {
 	"risk_tolerance": null
 }
 var banks = [] #array of dictionaries {"x":-10,"y":8,"z":3, "risk_tolerance":bankState}
-func create_bank(x_value, y_value, z_value, risk_tolerance_value: bankState)->void:
+func add_bank(x_value, y_value, z_value, risk_tolerance_value: bankState)->void:
 	var new_bank = bank_template.duplicate() # Duplicate the template to avoid modifying the original
 	new_bank["x"] = x_value
 	new_bank["y"] = y_value
@@ -39,6 +39,31 @@ var materials_factories = [] #array of dictionaries {"x":-10,"y":8, "z":3}
 var wind_turbines = [] #array of dictionaries {"x":-10,"y":8, "z":3}
 var forests = [] #array of dictionaries {"x":-10,"y":8, "z":3}
 
+var building_template = {
+	"x": null, 
+	"y": null, 
+	"z": null, 
+}
+
+func new_building_helper(x_value, y_value, z_value)->Dictionary:
+	var new_building = building_template.duplicate()
+	new_building["x"] = x_value
+	new_building["y"] = y_value
+	new_building["z"] = z_value
+	return new_building
+
+func add_apartment_building(x_value, y_value, z_value)->void:
+	var new_building=new_building_helper(x_value, y_value, z_value)
+	apartment_buildings.append(new_building)
+	
+func add_oil_pumps(x_value, y_value, z_value)->void:
+	var new_building=new_building_helper(x_value, y_value, z_value)
+	oil_pumps.append(new_building)
+	
+func add_materials_factories(x_value, y_value, z_value)->void:
+	var new_building=new_building_helper(x_value, y_value, z_value)
+	materials_factories.append(new_building)
+
 #Actions
 var times_ocean_cleaned = 0 
 
@@ -47,12 +72,31 @@ func clean_ocean()->void:
 	world_health+=50
 	times_ocean_cleaned+=1
 
+func normal_distribution(midpoint: float, stddev: float) -> float:
+	var u1 = randf()
+	var u2 = randf()
+	var z0 = sqrt(-2.0 * log(u1)) * cos(2.0 * PI * u2)
+	return z0 * stddev + midpoint
+
 #Dashboard-exclusive variables
 var energy_price = 500000 #income for selling 1GWH your energy at a particular time
 var oil_price = 500000 #income from selling 1MB
 var materials_price = 500000 #income from selling 1 unit of materials
-#VOLATITLIY FUNCTION TO UPDATE PRICES BASED ON NUMBER OF YOLO BANKS ETC
-#will also display some info from other variables above
+
+#FUNCTION TO UPDATE PRICES OF COMMODITIES BASED ON NUMBER OF YOLO BANKS ETC
+func update_comodities()->void:
+	var average=0
+	var total=0
+	for bank in banks:
+		total+=abs(risk_performance_map[bank.risk_tolerance_value])
+	#average of bank absolute performances is a proxy/indicator of overall volatility i.e. high volatility -> more skewed/extreme average -> can be used to set SD of normal distribution function that determines commodity prices
+	average=total/banks.size()
+	
+	#if your average bank performance is too extreme (further away from 0), then your commodity prices will also be extreme i.e. use it to update your normal distribution S.D.s
+	#maximum possible abs(average) will be 2500 so want to *100 and make it never go below 0
+	energy_price = max(normal_distribution(500000,abs(average))*100,0) #income for selling 1GWH your energy at a particular time
+	oil_price = max(normal_distribution(500000,abs(average))*100,0) #income from selling 1MB
+	materials_price = max(normal_distribution(500000,abs(average))*100,0) #income from selling 1 unit of materials
 
 #Deltas
 var money_per_time = 0 #from bank investment performance (n.b. extra money will come from selling oil/energy/materials)
@@ -64,14 +108,7 @@ var energy_per_time = 0 #from wind_turbines
 func _ready() -> void:
 	pass
 
-func normal_distribution(midpoint: float, stddev: float) -> float:
-	var u1 = randf()
-	var u2 = randf()
-	var z0 = sqrt(-2.0 * log(u1)) * cos(2.0 * PI * u2)
-	return z0 * stddev + midpoint
-
 func getUpdatedPerformance(risk_tol: bankState) -> int:
-	#Still need to make the logic for generating a random standard deviation for performance of stock. At moment just hardcoded
 	if(risk_tol==bankState.CONSERVATIVE):
 		return max(normal_distribution(3,1),0)
 	if(risk_tol==bankState.RISKY):
@@ -100,7 +137,8 @@ func sell_energy() -> void:
 		energy-=50
 
 var time_passed = 0.0 #in seconds
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Called every frame. 'delta' is the elapsed time since the previous frame
+
 func _process(delta: float) -> void:
 	time_passed += delta
 	
@@ -120,6 +158,9 @@ func _process(delta: float) -> void:
 			#use updated risk tolerance of each bank to update your money_per_time
 			money_per_time += risk_performance_map[bank.risk_tolerance]
 		
+		#update commodities after bank performance is updated
+		update_comodities()
+		
 		#resources values updates
 		money+=money_per_time
 		oil+=oil_per_time
@@ -127,3 +168,8 @@ func _process(delta: float) -> void:
 		energy+=energy_per_time
 		
 		
+func _unhandled_input(event: InputEvent) -> void:
+	if(Input.is_action_just_pressed("test")):
+		print(oil)
+		sell_oil()
+		print(oil)
