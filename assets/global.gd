4e90a0b224deff4c263541f34619dd5559bad64e
isym444 <samir.yep@gmail.com>
2024-10-12 15:45:23 +0100

    updated functions

--- assets/global.gd
+++ assets/global.gd
@@ -1,7 +1,9 @@
 extends Node
 
 #Constants
-const oil_per_factory = 5 #MB millions of barrels per time
+const oil_per_factory = 1 #MB millions of barrels per time
+const materials_per_factory = 5 #building units
+const energy_per_turbine = 5 #GW
 
 #Resources
 var money = 10000000 #GBP (10 million) #money can go down as non-residential buildings cost money to operate so they can operate at deficit if you don't sell your oil/energy or price of sell is too low
@@ -15,7 +17,22 @@ var time_survived = 0 #in seconds
 enum bankState {CONSERVATIVE, RISKY, YOLO}
 
 #Buildings
+var bank_template = {
+	"x": null, 
+	"y": null, 
+	"z": null, 
+	"risk_tolerance": null
+}
 var banks = [] #array of dictionaries {"x":-10,"y":8,"z":3, "risk_tolerance":bankState}
+func create_bank(x_value, y_value, z_value, risk_tolerance_value):
+	var new_bank = bank_template.duplicate() # Duplicate the template to avoid modifying the original
+	new_bank["x"] = x_value
+	new_bank["y"] = y_value
+	new_bank["z"] = z_value
+	new_bank["risk_tolerance"] = risk_tolerance_value
+	banks.append(new_bank)
+var risk_performance_map = {bankState.CONSERVATIVE:0, bankState.RISKY:0, bankState.YOLO:0}
+
 var apartment_buildings = [] #array of dictionaries {"x":-10,"y":8, "z":3}
 var oil_pumps = [] #array of dictionaries {"x":-10,"y":8, "z":3}
 var materials_factories = [] #array of dictionaries {"x":-10,"y":8, "z":3}
@@ -42,6 +59,9 @@ func _ready() -> void:
 
 var time_passed = 0.0 #in seconds
 
+func getUpdatedPerformance(risk_tol) -> int:
+	return 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	time_passed += delta
@@ -53,8 +73,13 @@ func _process(delta: float) -> void:
 		
 		#update Deltas based on number of buildings
 		oil_per_time = oil_pumps.size()*oil_per_factory
-		materials_per_time
-		money_per_time
+		materials_per_time = materials_factories.size()*materials_per_factory
+		energy_per_time = wind_turbines.size()*energy_per_turbine
+		
+		for bank in banks:
+			#update risk tolerance
+			risk_performance_map[bank.risk_tolerance]=getUpdatedPerformance(bank.risk_tolerance)
+			money_per_time += risk_performance_map[bank.risk_tolerance]
 		
 		#resources values updates
 		money+=money_per_time
