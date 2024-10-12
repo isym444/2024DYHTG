82a3e3abd7cc7219fcd45d8ccbc16670b5f98438
Acul4321 <lucarogers666@gmail.com>
2024-10-12 13:46:06 +0100

    building placment

--- script/main.gd
+++ script/main.gd
@@ -3,7 +3,9 @@ extends Node3D
 @export var selector:Node3D # The 'cursor'
 @export var view_camera:Camera3D # Used for raycasting mouse
 
+var bounds = 10
 var plane:Plane # Used for raycasting mouse
+var plane_position #used for getting coord
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,5 +19,13 @@ func _process(delta):
 		view_camera.project_ray_origin(get_viewport().get_mouse_position()),
 		view_camera.project_ray_normal(get_viewport().get_mouse_position()))
 		
-	var gridmap_position = Vector3(round(world_position.x), 0, round(world_position.z))
-	selector.position = lerp(selector.position, gridmap_position, delta * 40)
+	plane_position = Vector3(round(world_position.x), 0, round(world_position.z))
+	selector.position = lerp(selector.position, plane_position, delta * 40)
+
+func _unhandled_input(event):
+	if(Input.is_action_just_pressed("click")):
+		print(plane_position)
+		var building = load("res://scene/building/residential.tscn").instantiate()
+		building.global_position = plane_position
+		# Add the TextureRect to the scene
+		add_child(building)
