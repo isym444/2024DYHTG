2b75dd249d98e670f59d247c0dbb5a37b1192809
isym444 <samir.yep@gmail.com>
2024-10-13 04:59:22 +0100

    made dashboard functionality

--- script/main.gd
+++ script/main.gd
@@ -27,6 +27,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	%FullDashboard.visible = global.dashboardVis
 	var world_position = plane.intersects_ray(
 		view_camera.project_ray_origin(get_viewport().get_mouse_position()),
 		view_camera.project_ray_normal(get_viewport().get_mouse_position()))
